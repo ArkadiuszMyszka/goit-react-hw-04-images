dde3620931f6d0f2dd68e83baa72d726e0a383bb
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-08-18 06:26:55 +0200

    Gallery done

--- src/components/ImageGallery/index.jsx
+++ src/components/ImageGallery/index.jsx
@@ -1,46 +1,33 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem';
 import styles from './ImageGallery.module.css';
 import Modal from 'components/Modal';
 
-export class ImageGallery extends Component {
-  static propTypes = { images: PropTypes.arrayOf(PropTypes.object) };
+const ImageGallery = ({ images }) => {
+  const [largeImg, setLargeImg] = useState('');
+  // const [index, setIndex] = useState(-1);
 
-  state = {
-    largeImg: '',
-    index: -1,
+  const handleClick = largeImg => {
+    setLargeImg(largeImg);
   };
 
-  handleClick = (largeImg, index) => {
-    this.setState({
-      largeImg,
-      index,
-    });
-  };
-
-  render() {
-    const { images } = this.props;
-    const { largeImg } = this.state;
-    return (
-      <>
-        <ul className={styles.container}>
-          {images.map(({ id, webformatURL, largeImageURL }, i) => {
-            return (
-              <ImageGalleryItem
-                key={`${id}${i}`}
-                src={webformatURL}
-                onClick={() => this.handleClick(largeImageURL, i)}
-              />
-            );
-          })}
-        </ul>
-        {largeImg && (
-          <Modal largeImage={largeImg} onClickExit={this.handleClick} />
-        )}
-      </>
-    );
-  }
-}
-
+  return (
+    <>
+      <ul className={styles.container}>
+        {images.map(({ id, webformatURL, largeImageURL }, i) => {
+          return (
+            <ImageGalleryItem
+              key={`${id}${i}`}
+              src={webformatURL}
+              onClick={() => handleClick(largeImageURL)}
+            />
+          );
+        })}
+      </ul>
+      {largeImg && <Modal largeImage={largeImg} onClickExit={handleClick} />}
+    </>
+  );
+};
+ImageGallery.propTypes = { images: PropTypes.arrayOf(PropTypes.object) };
 export default ImageGallery;
