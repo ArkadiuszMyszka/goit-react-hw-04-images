ad1fa3a5a0da21ed3b12f21fe5bbfc70f03612cd
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-08-18 06:41:30 +0200

    galleryItem done

--- src/components/ImageGalleryItem/index.jsx
+++ src/components/ImageGalleryItem/index.jsx
@@ -1,17 +1,12 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-
 import styles from './ImageGalleryItem.module.css';
 
-class ImageGalleryItem extends Component {
-  static propTypes = { src: PropTypes.string, onClick: PropTypes.func };
+const ImageGalleryItem = ({ src, largeSrc, onClick }) => {
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(false);
 
-  state = {
-    loaded: false,
-    error: false,
-  };
-
-  scrollLock = () => {
+  const scrollLock = () => {
     const documentWidth = document.documentElement.clientWidth;
     const windowWidth = window.innerWidth;
     const scrollBarWidth = windowWidth - documentWidth;
@@ -19,45 +14,45 @@ class ImageGalleryItem extends Component {
     document.documentElement.classList.add('active-modal');
     document.body.style.paddingRight = `${scrollBarWidth}px`;
   };
-  handleClick = () => {
-    const { onClick } = this.props;
 
-    this.scrollLock();
+  const handleClick = () => {
+    scrollLock();
     onClick();
   };
-  handleLoad = () => {
-    this.setState({ loaded: true });
+  const handleLoad = () => {
+    setLoaded(false);
   };
-  handleError = () => {
-    this.setState({ error: true });
+  const handleError = () => {
+    setError(true);
   };
-  
-  render() {
-    const { src, largeSrc } = this.props;
-    const { loaded, error } = this.state;
-    return (
-      <>
-        {error ? (
-          <li className={styles.placeholder}></li>
-        ) : (
-          <li
-            className={loaded ? `${styles.container}` : `${styles.placeholder}`}
-          >
-            <img
-              className={styles.image}
-              style={loaded ? {} : { visibility: 'hidden' }}
-              src={src}
-              data-src={largeSrc}
-              alt=""
-              onLoad={this.handleLoad}
-              onError={this.handleError}
-              onClick={this.handleClick}
-            />
-          </li>
-        )}
-      </>
-    );
-  }
-}
 
+  return (
+    <>
+      {error ? (
+        <li className={styles.placeholder}></li>
+      ) : (
+        <li
+          className={loaded ? `${styles.container}` : `${styles.placeholder}`}
+        >
+          <img
+            className={styles.image}
+            style={loaded ? {} : { visibility: 'hidden' }}
+            src={src}
+            data-src={largeSrc}
+            alt=""
+            onLoad={handleLoad}
+            onError={handleError}
+            onClick={handleClick}
+          />
+        </li>
+      )}
+    </>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  src: PropTypes.string,
+  largeSrc: PropTypes.string,
+  onClick: PropTypes.func,
+};
 export default ImageGalleryItem;
